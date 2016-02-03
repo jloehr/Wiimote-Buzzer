8591fdc6960e73eab8bade5ed8c8eef6bcb2c1d8
Julian Löhr <julian.loehr@outlook.com>
2016-02-03 13:04:20 +0100

    Create Projects

---
+++ Wiimote Buzzer Lib/Wiimote Buzzer Lib.h
@@ -0,0 +1,13 @@
+// Wiimote Buzzer Lib.h
+
+#pragma once
+
+using namespace System;
+
+namespace WiimoteBuzzerLib {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
