d404519f77cc9427d42c7148e47ca8e5c609aca5
Julian Löhr <jloehr_1@hotmail.com>
2016-02-04 18:20:39 +0100

    Fix LEDs and add "ALL"

--- Wiimote Buzzer Lib/Wiimote.h
+++ Wiimote Buzzer Lib/Wiimote.h
@@ -9,7 +9,7 @@ namespace WiimoteBuzzerLib {
 	public ref class Wiimote
 	{
 	public:
-		enum class WiimoteLED { LED1 = 0x10, LED2 = 0x20, LED3 = 0x30, LED4 = 0x40 };
+		enum class WiimoteLED { LED1 = 0x10, LED2 = 0x20, LED3 = 0x40, LED4 = 0x80, ALL = 0xF0 };
 
 		Wiimote(const WiimoteScanner::WiimoteData & DeviceData);
 		~Wiimote();
